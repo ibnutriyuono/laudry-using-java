d817e0feef48342d4113e9abdbe8cb273b7a16ff
ibnutriyuono <ibnutriyuono@gmail.com>
2018-01-10 16:04:54 +0700

    adding menu

---
+++ Laundry/src/mainprogram/main.java
@@ -0,0 +1,25 @@
+package mainprogram;
+
+import admin.*;
+import customers.*;
+import java.util.*;
+
+/**
+ *
+ * @author mity
+ */
+public class main {
+    public static void main (String [] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("PROGRAM APLIKASI LAUNDRY");
+        System.out.println("Silahkan pilih menu berikut");
+        System.out.println("1. Admin");
+        System.out.println("2. Customer");
+        System.out.println("0. Exit Program");
+        String pil = scan.next();
+//        lihatdata.main(args);
+        
+    }
+}
